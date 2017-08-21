0e31d0bac900a64cff616f07d1c85223b4d69111
nnkhoa <n.nhukhoa2209@gmail.com>
2017-08-21 14:24:37 +0700

    Intel compiler environment variables for RegCM compilation

---
+++ exportVar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export CC=icc
+export CXX=icpc
+export FC=ifort
+export F77=ifort
+export F90=ifort
+export I_MPI_CC=icc
+export I_MPI_CXX=icpc
+export I_MPI_FC=ifort
+export I_MPI_F77=ifort
+export I_MPI_F90=ifort
